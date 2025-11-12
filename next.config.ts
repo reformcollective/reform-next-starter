726d4cf4765f487074d038e90924b0f0a05eaf29
Robbie <robbiethewadley@gmail.com>
2025-11-12 11:06:06 -0700

    packages

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,15 @@
-import bundleAnalyzer from "@next/bundle-analyzer"
 import type { NextConfig } from "next"
 import { serverSiteURL } from "./app/library/siteURL/determine"
-
-const withBundleAnalyzer = bundleAnalyzer({
-	enabled: process.env.ANALYZE === "true",
-})
 
 const nextConfig: NextConfig = {
 	redirects: async () => [
 		{ source: "/home", destination: "/", permanent: false },
 	],
-
+	reactCompiler: { panicThreshold: "all_errors" },
 	experimental: {
-		reactCompiler: { panicThreshold: "ALL_ERRORS" },
 		viewTransition: true,
 	},
 	env: {
-		// Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
-		SC_DISABLE_SPEEDY: "false",
 		NEXT_PUBLIC_DEPLOY_URL: serverSiteURL,
 	},
 
@@ -39,4 +31,4 @@ const nextConfig: NextConfig = {
 	},
 }
 
-export default withBundleAnalyzer(nextConfig)
+export default nextConfig
