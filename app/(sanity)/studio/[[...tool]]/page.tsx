2c1ff0ac024f7d3e1c669d2814c5017cbccc0c0b
Robbie <robbiethewadley@gmail.com>
2025-11-13 14:35:49 -0700

    package cleanup

--- app/(sanity)/studio/[[...tool]]/page.tsx
+++ app/(sanity)/studio/[[...tool]]/page.tsx
@@ -1,18 +1,14 @@
 "use client"
 
-import { Portal } from "@ariakit/react"
+import Portal from "library/Portal"
 import { NextStudio } from "next-sanity/studio"
 import config from "@/sanity.config"
-
 import "./style.css"
-import { Suspense } from "react"
 
 export default function StudioPage() {
 	return (
-		<Suspense>
-			<Portal>
-				<NextStudio config={config} />
-			</Portal>
-		</Suspense>
+		<Portal>
+			<NextStudio config={config} />
+		</Portal>
 	)
 }
---
+++ sanity/lib/types.ts
@@ -0,0 +1,2 @@
+// re-export all generated sanity types from the root so imports can use `sanity/lib/types`
+export * from "../../sanity.types"
