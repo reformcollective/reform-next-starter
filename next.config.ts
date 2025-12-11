84614d5f2329cbf00f4a1c80634761b6f289cf9e
Robbie <robbiethewadley@gmail.com>
2025-12-11 15:20:55 -0700

    configure next caching

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  turbopack: {
+    // turbopack must be able to resolve packages from pnpm's global store
+    root: "/",
+  },
+
+  experimental: {
+    turbopackFileSystemCacheForBuild: true,
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
