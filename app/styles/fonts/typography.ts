20a4efec099e2220b2a5f85ff85d30754f9257e8
Robbie <robbiethewadley@gmail.com>
2025-12-12 12:30:16 -0700

    figma oriented font setup

--- app/styles/fonts/typography.ts
+++ app/styles/fonts/typography.ts
@@ -1,30 +1,33 @@
-import localFont from "next/font/local"
+import { Geist } from "next/font/google"
 
-// after setup, verify the font & font spacing looks the same across all browsers (and windows/mac/linux if you can)
-
-export const roboto = localFont({
-  adjustFontFallback: "Arial",
+export const geist = Geist({
+  subsets: ["latin"],
   display: "swap",
-  src: [
-    {
-      path: "./Roboto-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./Roboto-Italic.woff2",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./Roboto-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./Roboto-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-  ],
 })
+
+// export const exampleFont = localFont({
+//   adjustFontFallback: "Arial",
+//   display: "swap",
+//   src: [
+//     {
+//       path: "./Roboto-Regular.woff2",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Roboto-Italic.woff2",
+//       weight: "400",
+//       style: "italic",
+//     },
+//     {
+//       path: "./Roboto-Bold.woff2",
+//       weight: "700",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Roboto-BoldItalic.woff2",
+//       weight: "700",
+//       style: "italic",
+//     },
+//   ],
+// })
